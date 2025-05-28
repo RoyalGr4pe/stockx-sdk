2759436177c23c44ddd700ff55d882b2ad821420
RoyalGr4pe <njames.programming@gmail.com>
2025-05-29 00:23:21 +0100

    fix secure call

--- src/stockx/secure.py
+++ src/stockx/secure.py
@@ -5,10 +5,10 @@ def secure_call():
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
-                return func(args, kwargs)
+                return func(*args, **kwargs)
             except Exception as error:
                 return {"error": error, "trackback": traceback.format_exc()}
 
         return wrapper
-    
+
     return decorator
